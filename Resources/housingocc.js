c489597ade6dfc6f5a2d10201720a9057df627d1
kaylajgranados <kaylajgranados@gmail.com>
2022-11-03 22:08:52 -0500

    pie chart code

--- Resources/housingocc.js
+++ Resources/housingocc.js
@@ -1,1 +1,27 @@
-// test 
+// code for bringing in url 
+
+// code for choosing which data - just the occupancy df 
+
+onst data = {
+    labels: [
+      'Red',
+      'Blue',
+      'Yellow'
+    ],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [300, 50, 100],
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(54, 162, 235)',
+        'rgb(255, 205, 86)'
+      ],
+      hoverOffset: 4
+    }]
+  };
+  
+  const config = {
+    type: 'pie',
+    data: data,
+  };
+  
