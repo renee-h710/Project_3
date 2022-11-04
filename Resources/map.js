488604f73eebc69449a0d9cc9d89da5486a987d8
mariahart1 <mariahart1@yahoo.com>
2022-11-03 19:13:42 -0700

    javascript files

---
+++ Resources/map.js
@@ -0,0 +1,14 @@
+// Creating our initial map object:
+// We set the longitude, latitude, and starting zoom level.
+// This gets inserted into the div with an id of "map".
+var myMap = L.map("map", {
+    center: [45.52, -122.67],
+    zoom: 13
+  });
+  
+  // Adding a tile layer (the background map image) to our map:
+  // We use the addTo() method to add objects to our map.
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(myMap);
+  
