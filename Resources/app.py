5bba4da09f5e20a40f53f7dfaa8d616feaa129b7
Renee Holliday <renee.holliday710@gmail.com>
2022-11-07 19:10:44 -0800

    modified:   Resources/app.py 	modified:   Resources/housingocc.js

--- Resources/app.py
+++ Resources/app.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
--- Resources/housingocc.js
+++ Resources/housingocc.js
@@ -1,27 +1,62 @@
 // code for bringing in url 
-
+let url = 'Real-Estate-Data.azurewebsites.net'
 // code for choosing which data - just the occupancy df 
+let occupancySet = '/api/v1.0/occupancy_per'
 
-onst data = {
-    labels: [
-      'Red',
-      'Blue',
-      'Yellow'
-    ],
-    datasets: [{
-      label: 'My First Dataset',
-      data: [300, 50, 100],
-      backgroundColor: [
-        'rgb(255, 99, 132)',
-        'rgb(54, 162, 235)',
-        'rgb(255, 205, 86)'
-      ],
-      hoverOffset: 4
-    }]
+query_url = url + occupancySet;
+
+//Pull data from API and transform into lists
+function getData(query_url){
+  let response = fetch(query_url)
+  let states = response.json();
+  return states;
+}
+
+let occupiedField = "occupied";
+let vacantField = "vacant";
+
+//Get averages of data to use for plot, using metric for either the occupied or vacant list
+function getMean(states, metric){
+  // Initialize variables to increment
+  let count = 0;
+  let total = 0;
+  
+
+  //loop through each state to get sum and count of occupied or vacant list
+  for (let i = 0; i<states.length;i++){
+    row = states[i];
+
+    total += row[metric];
+    count += row[metric];
+  }
+  // Calculate the average value
+  let meanValue = total / count;
+
+  // Return the calculated average
+  return meanValue;
+}
+
+//Build the plot
+function createPie(occupiedAverage, vacantAverage){
+  var data = [{
+    values: [occupiedAverage, vacantAverage],
+    labels: ['Occupied', 'Vacant'],
+    type: 'pie',
+    name: 'Average Houses Occupied vs. Vacant',
+    hoverinfo: 'label+value'
+  }];
+  
+  var layout = {
+    height: 400,
+    width: 500
   };
   
-  const config = {
-    type: 'pie',
-    data: data,
-  };
+  Plotly.newPlot('pie', data, layout);
+
+
+}
+
+//Create plot for site
+createPie(getMean(getData(query_url),occupiedField),getMean(getData(query_url),vacantField));
+
   
