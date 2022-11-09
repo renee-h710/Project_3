851a1ff914b6254129dc48e4a58e9bb48091bfd5
Renee Holliday <renee.holliday710@gmail.com>
2022-11-08 17:59:02 -0800

    modified:   Resources/housingocc.js 	modified:   app.py 	new file:   appRequirements.zip 	deleted:    appService.zip 	modified:   occupancy.html 	modified:   requirements.txt

--- Resources/housingocc.js
+++ Resources/housingocc.js
@@ -1,69 +1,94 @@
 // code for bringing in url 
-let url = 'Real-Estate-Data.azurewebsites.net'
+let url = 'https://Real-Estate-Data.azurewebsites.net'
 // code for choosing which data - just the occupancy df 
 let occupancySet = '/api/v1.0/occupancy_per'
 
 query_url = url + occupancySet;
 
-//Pull data from API and transform into lists
-function getData(query_url){
-  let response = fetch(query_url)
-  let states = response.json();
-  return states;
-}
 
 
-let occupiedField = "occupied";
-let vacantField = "vacant";
-
-//Get averages of data to use for plot, using metric for either the occupied or vacant list
-function getMean(states, metric){
-  // Initialize variables to increment
-  let count = 0;
-  let total = 0;
+function init() {
+  //Create mean startup plot for site
+  vacantList = [];
+  occupiedList = [];
   
+  d3.json(query_url, function(data){
+  states = data.state
+  vacantList = data.vacant
+  occupiedList = data.occupied
+  })
+  occMean = d3.mean(occupiedList)
+  vacMean = d3.mean(vacantList)
 
-  //loop through each state to get sum and count of occupied or vacant list
-  for (let i = 0; i<states.length;i++){
-    row = states[i];
-
-    total += row[metric];
-    count += row[metric];
-  }
-  // Calculate the average value
-  let meanValue = total / count;
-
-  // Return the calculated average
-  return meanValue;
-}
-
-//Build the plot
-function createPie(occupiedAverage, vacantAverage){
   var data = [{
-    values: [occupiedAverage, vacantAverage],
+    values: [occMean, vacMean],
     labels: ['Occupied', 'Vacant'],
     type: 'pie',
     name: 'Average Houses Occupied vs. Vacant',
-    hoverinfo: 'label+value'
+
   }];
-  
-  var layout = {
-    height: 400,
-    width: 500
-  };
-  
-// Title 
-let layout = {
-  title: "Average Occupancy vs Vacant Units in US" 
-};
 
-
-  Plotly.newPlot('pie', data, layout);
-
-
+  Plotly.newPlot("pie", data);
 }
 
-//Create plot for site
-createPie(getMean(getData(query_url),occupiedField),getMean(getData(query_url),vacantField));
+init();
 
+//Pull data from API and transform into lists
+// function getData(query_url){
+//     let response = fetch(query_url)
+//     let states = response.json();
+//     return states;
+// }
+
+// let occupiedField = "occupied";
+// let vacantField = "vacant";
+
+// //Get averages of data to use for plot, using metric for either the occupied or vacant list
+// function getMean(occupied, vacant){
+//   // Initialize variables to increment
+//   let occCount = occupied.length;
+//   let vacCount = vacant.length;
+//   let total = 0;
   
+
+//   //loop through each state to get sum and count of occupied or vacant list
+//   for (let i = 0; i<occupied.length;i++){
+//     row = occupied[i];
+
+//     total += row[metric];
+//     count += row[metric];
+//   }
+//   // Calculate the average value
+//   let meanValue = total / count;
+
+//   // Return the calculated average
+//   return meanValue;
+// }
+
+// //Build the plot
+// function createPie(occupiedAverage, vacantAverage){
+//   var data = [{
+//     values: [occupiedAverage, vacantAverage],
+//     labels: ['Occupied', 'Vacant'],
+//     type: 'pie',
+//     name: 'Average Houses Occupied vs. Vacant',
+
+//   }];
+  
+//   var layout = {
+//     height: 400,
+//     width: 500
+//   };
+  
+// // Title 
+// let layout = {
+//   title: "Average Occupancy vs Vacant Units in US" 
+// };
+
+
+//   Plotly.newPlot('pie', data, layout);
+
+
+// }
+
+
--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
-
+from flask_cors import CORS
 
 #################################################
 # Database Setup
